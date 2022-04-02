5ffdd20e2679506142061f8e28a8c7d63364cd1c
Andrea Mac <aleatrengrouse@gmail.com>
2022-04-03 00:10:04 +0100

    file pathing wasnt working

--- controllers/home-Routes.js
+++ controllers/home-Routes.js
@@ -11,12 +11,12 @@ router.get("/", (req, res) => {
         attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
         include: {
           model: User,
-          attributes: ["username"],
+          attributes: ["email"],
         },
       },
       {
         model: User,
-        attributes: ["username"],
+        attributes: ["email"],
       },
     ],
   })
