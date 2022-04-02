08e19cf91dd61ffbe1097ceec4971e916bd80888
Andrea Mac <aleatrengrouse@gmail.com>
2022-04-02 22:45:23 +0100

    fixing routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,15 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const apiRoutes = require('./api');
-const homeRoutes = require('./homeRoutes');
+const apiRoutes = require("./api");
+const homeRoutes = require("./homeRoutes");
+const userRoutes = require("./user-routes.js");
+const postRoutes = require("./post-routes");
+const commentRoutes = require("./comment-routes");
 
-router.use('/', homeRoutes);
-router.use('/api', apiRoutes);
+router.use("/", homeRoutes);
+router.use("/api", apiRoutes);
+router.use("/users", userRoutes);
+router.use("/posts", postRoutes);
+router.use("/comments", commentRoutes);
 
 module.exports = router;
